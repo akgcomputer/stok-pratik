93fa0476a53496a2dc50f1ac855e691bc0d2df96
isdeyeter <edestechbilisim@gmail.com>
2026-07-18 02:24:55 +0300

    fix: restore Keystatic API route for GitHub Auth

---
+++ src/pages/api/keystatic/[...params].ts
@@ -0,0 +1,6 @@
+import { makeAPIRouteHandler } from '@keystatic/astro/api';
+import keystaticConfig from '../../../../keystatic.config';
+
+export const all = makeAPIRouteHandler({
+  config: keystaticConfig,
+});
